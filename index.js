8bf1b0378779675ffc3abd790782ac0ce6379bec
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-11-09 14:22:26 -0500

    completed action endpoints, updated validation middleware

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ const logger = (req, res, next) => {
   next();
 };
 
+// START MIDDLEWARE //
+
 const validateID = (req, res, next) => {
   const { id } = req.params;
   const type = req.url.includes("projects") ? "project" : "action";
@@ -43,6 +45,23 @@ const validateID = (req, res, next) => {
     : type === "action"
     ? (helperCall = aDB)
     : null;
+
+  if (req.method === "POST" && type === "action") {
+    pDB
+      .get(id)
+      .then(result =>
+        !result
+          ? res.status(400).json({
+              error: "There is no post associated with the supplied ID."
+            })
+          : next()
+      )
+      .catch(err =>
+        res.status(500).json({
+          error: "There was an error finding this post. Please try again."
+        })
+      );
+  }
 
   helperCall.get(id).then(result => {
     !result
@@ -56,17 +75,31 @@ const validateID = (req, res, next) => {
 
 const validateProject = (req, res, next) => {
   const newProj = req.body;
-  !req.body.name || !req.body.description
+  !newProj.name || !newProj.description
     ? res
         .status(400)
         .json({ error: "Project must have a name and description." })
     : next();
 };
 
+const validateAction = (req, res, next) => {
+  const newAction = req.body;
+
+  !newAction.description || !newAction.notes
+    ? res
+        .status(400)
+        .json({ error: "Action must have a description and notes." })
+    : next();
+};
+
+// END MIDDLEWARE //
+
 server.use(express.json());
 server.use(cors());
 
-server.get("/projects", (req, res) => {
+// START PROJECT ENDPOINTS //
+
+server.get("/projects", logger, (req, res) => {
   pDB
     .get()
     .then(projects => res.status(200).json({ projects: projects }))
@@ -77,7 +110,7 @@ server.get("/projects", (req, res) => {
     );
 });
 
-server.get("/projects/:id", validateID, (req, res) => {
+server.get("/projects/:id", logger, validateID, (req, res) => {
   pDB
     .get(req.identifier)
     .then(project => res.status(200).json({ project: project }))
@@ -88,7 +121,19 @@ server.get("/projects/:id", validateID, (req, res) => {
     );
 });
 
-server.post("/projects", validateProject, (req, res) => {
+server.get("/project-actions/:id", logger, validateID, (req, res) => {
+  pDB
+    .getProjectActions(req.identifier)
+    .then(actions => res.status(200).json({ actions: actions }))
+    .catch(err =>
+      res.status(500).json({
+        error:
+          "Could not retrieve actions associated with the project. Please try again."
+      })
+    );
+});
+
+server.post("/projects", logger, validateProject, (req, res) => {
   pDB
     .insert({
       name: req.body.name,
@@ -99,11 +144,11 @@ server.post("/projects", validateProject, (req, res) => {
     .catch(err =>
       res
         .status(500)
-        .json({ error: "Could not create new proect. Please try again." })
+        .json({ error: "Could not create new project. Please try again." })
     );
 });
 
-server.put("/projects/:id", validateID, (req, res) => {
+server.put("/projects/:id", logger, validateID, (req, res) => {
   pDB
     .update(req.identifier, req.body)
     .then(project => res.status(200).json({ project: project }))
@@ -114,15 +159,81 @@ server.put("/projects/:id", validateID, (req, res) => {
     );
 });
 
-server.delete("/projects/:id", validateID, (req, res) => {
+server.delete("/projects/:id", logger, validateID, (req, res) => {
   pDB
     .remove(req.identifier)
     .then(project => res.status(204).end())
     .catch(err =>
+      res.status(500).json({
+        error: "Could not delete the requested project. Please try again."
+      })
+    );
+});
+
+// END PROJECT ENDPOINTS //
+
+// START ACTION ENDPOINTS //
+
+server.get("/actions", logger, (req, res) => {
+  aDB
+    .get()
+    .then(actions => res.status(200).json({ actions: actions }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "Sorry, cannot retrieve actions at this time." })
+    );
+});
+
+server.get("/actions/:id", logger, validateID, (req, res) => {
+  aDB
+    .get(req.identifier)
+    .then(action => res.status(200).json({ action: action }))
+    .catch(err =>
+      res.status(500).json({
+        error: `There was a problem getting this action. Please try again.`
+      })
+    );
+});
+
+server.post("/actions/:id", logger, validateID, validateAction, (req, res) => {
+  aDB
+    .insert({
+      project_id: req.identifier,
+      description: req.body.description,
+      notes: req.body.notes,
+      completed: false
+    })
+    .then(action => res.status(201).json({ action: action }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "Could not create new action. Please try again." })
+    );
+});
+
+server.put("/actions/:id", logger, validateID, (req, res) => {
+  aDB
+    .update(req.identifier, req.body)
+    .then(action => res.status(200).json({ action: action }))
+    .catch(err =>
+      res
+        .status(500)
+        .json({ err: "Could not update action. Please try again." })
+    );
+});
+
+server.delete("/actions/:id", logger, validateID, (req, res) => {
+  aDB
+    .remove(req.identifier)
+    .then(action => res.status(204).end())
+    .catch(err =>
       res
         .status(500)
         .json({
-          error: "Could not delete the requested project. Please try again."
+          error: "Could not delete the requested action. Please try again."
         })
     );
 });
+
+// END ACTION ENDPOINTS //
